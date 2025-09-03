11b36b7e254980025bfadddc9161a666effcb122
Mrunali <mrunalijibhakate04@gmail.com>
2025-09-03 09:38:46 +0530

    initial commit

---
+++ 00-basic/a1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    int count = 0;   // keeps track of balance
+
+    printf("Enter parentheses string: ");
+    scanf("%s", s);
+}
