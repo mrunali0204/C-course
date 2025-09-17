0c81822aea276b1ee7773b24672230a71730da40
Mrunali <mrunalijibhakate04@gmail.com>
2025-09-17 10:28:21 +0530

    updated code

--- 01-intro/01-calculation/calculation.c
+++ 01-intro/01-calculation/calculation.c
@@ -1,34 +1,72 @@
-int modulus(int, int);
-int multiplication(void);
-void substraction(int, int);
-void division(void);
-int addition(int, int);
-
-# include <stdio.h>
-# include "calculation.h"
+#include<stdio.h>
+#include "calculation.h"
 
 int main(void)
 {
-    int modulus(int, int);
-    int multiplication(void);
-    void substraction(int, int);
-    void division(void);
-    int addition(int, int);
+    int no1;
+    int no2;
+    int ans;
+
+    printf("enter the two number\n: ");
+    scanf("%d%d", &no1, &no2);
+
+    ans = addition(no1, no2);
+    printf("addition is %d\n: ",ans);
+
+    substraction(no1, no2);
+
+    printf("multiplication is %d\n: ", multiplication());
+
+    division();
+
+    printf("enter numberator and denominator:\t");
+    scanf("%d%d", &no1, &no2);
+    ans = modulus(no1, no2);
+    printf("remainder is %d\n", ans);
+
+    return 0;
 
 }
 
-int modulus(int, int)
+int modulus(int num, int den)
 {
-    int no1 = 2;
-    int no2 = 3;
-
-    return no1 % no2;
+    int remainder;
+    remainder = num % den;
+    return remainder;
 }
 
 int multiplication(void)
 {
-    printf("enter the first number%d\n: ", no1);
-    printf("enter the second number%d\n: ", no2);
+    int no1;
+    int no2;
+
+    printf("enter the two numbers\n: ");
     scanf("%d%d", &no1, &no2);
-    
+
+    return no1 * no2;
+}
+
+void substraction(int no1, int no2)
+{
+    int ans;
+    ans = no1 - no2;
+    printf("substraction: %d - %d = %d\n", no1, no2, ans);
+}
+
+void division(void)
+{
+    int numberator;
+    int denominator;
+
+    printf("\nEnter numberator:\t ");
+    scanf("%d", &numberator);
+    printf("\nEnter denominator:\t ");
+    scanf("%d", &denominator);
+
+    printf("Quotient is %d\n: ", numberator / denominator);
+}
+
+int addition(int no1, int no2)
+{
+    return no1 + no2;
 }
