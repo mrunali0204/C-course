cae86391faba43e190925e3d81d6cd45a648e7ad
Mrunali <mrunalijibhakate04@gmail.com>
2025-10-07 20:31:29 +0530

    .

--- 02-operators/06-promotion demotion/promotion_demotion.c
+++ 02-operators/06-promotion demotion/promotion_demotion.c
@@ -12,11 +12,14 @@ int main(void)
 /*
 stepwise way of promotion and demotion:
 
-iAns = ( 2 * 3.25 + (9/2) );  
+          iAns = ( 2 * 3.25 + (9/2) );  
 
 step1:    iAns = ( (2.00 * 3.25) + (9/2) )      2 -> 2.00     promotion of 2 to 2.00
 step2:    iAns = ( (6.50) + (9/2) )  
-step3:    iAns = ( 6.50 + 4 )                   4 -> 4.00     promotion of 4 to 4.00
-step4:    iAns = 10.50                          10.50 -> 10     demotion of 10.50 to 10
 
+step3:    iAns = ( 6.50 + 4 )                   
+step4:    iAns = ( 6.50 + 4.00 )               4 -> 4.00     promotion of 4 to 4.00
+
+step5:    iAns = 10.50                          10.50 -> 10     demotion of 10.50 to 10
+step6:    iAns = 10                         10.50 -> 10     demotion of 10.50 to 10
 */
