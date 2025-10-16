383556c7aac88ba5bab21d2f110df27f15646f3f
Mrunali <mrunalijibhakate04@gmail.com>
2025-10-16 20:04:21 +0530

    .

--- 02-operators/15-extra assignment operator/assignment.c
+++ 02-operators/15-extra assignment operator/assignment.c
@@ -2,5 +2,20 @@
 
 int main(void)
 {
-    iNo = 10;
+    int iNo = 10;                    // R -> L assignment   
+
+    //iNo1 = iNo2 = iNo3 = 10;  // R -> L associativity      -> here if L -> R associativity then iNo3 would have binded to iNo2 so 10 value would be useless and no assignment would occur
+
+    iNo += 10; 
+    iNo = iNo + 10;
+
+    iNo -= 10; 
+    iNo = iNo - 10;
+
+    iNo &= 10; 
+    iNo = iNo & 10;
+
+    iNo *= 10; 
+    iNo = iNo * 10;
+
 }
