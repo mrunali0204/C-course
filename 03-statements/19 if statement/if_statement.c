2f66c3c37c962d8570caccd1a8c7abd10b4dc91d
Mrunali <mrunalijibhakate04@gmail.com>
2025-11-01 23:00:13 +0530

    .

---
+++ 03-statements/19 if statement/if_statement.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int iNo;
+
+    iNo = 5;
+    if(iNo < 10)
+        printf("1 One\n");          //One, Two
+        printf("2 Two\n");
+
+
+    iNo = 15;
+    if(iNo < 10)
+        printf("3 One\n");         // Two
+        printf("4 Two\n");
+
+
+    iNo = 5;
+    if(iNo < 10)
+    {
+        printf("5 One\n");        //One, Two
+        printf("6 Two\n");
+    }
+
+
+    iNo = 15;
+    if(iNo < 10)
+    {
+        printf("7 One\n");        //no output
+        printf("8 Two\n");
+    }
+
+
+    iNo = 15;
+    if(iNo < 10);
+    {
+        printf("9 One\n");        //One, Two
+        printf("10 Two\n");
+    }
+
+    return 0;
+}
+/*
+1 One
+2 Two
+4 Two
+5 One
+6 Two
+9 One
+10 Two
+*/
