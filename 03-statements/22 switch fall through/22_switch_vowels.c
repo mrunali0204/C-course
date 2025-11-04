bba91e0c727c8fa29c3e3680fb36b0fe12445935
Mrunali <mrunalijibhakate04@gmail.com>
2025-11-04 21:31:19 +0530

    .

---
+++ 03-statements/22 switch fall through/22_switch_vowels.c
@@ -0,0 +1,40 @@
+int main(void)
+{
+    char chChar;
+    printf("Enter characters: \n"); 
+    scanf("%c", &chChar); 
+
+    switch(chChar)
+    {
+        case 'a': 
+        case 'e': 
+        case 'i': 
+        case 'o': 
+        case 'u': 
+        printf("It is small letter vowel\n");                   
+        break;
+
+        case 'A': 
+        case 'E': 
+        case 'I': 
+        case 'O': 
+        case 'U': 
+        printf("It is capital letter vowel\n");                   
+        break;
+
+        default:
+        printf("It is not vowel\n"); 
+    }
+        return 0;
+    }
+
+/*
+Enter characters:
+a
+It is small letter vowel
+
+Enter characters:
+A
+It is capital letter vowel
+
+*/
